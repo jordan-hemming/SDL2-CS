282e792fa8d5a746b4c20f625aff421352f109d9
Jordan Hemming <jordan.hemming@outlook.com>
2024-05-07 10:08:19 +0100

    - Removed apparently incorrect overloads to RenderCopyEx + Added missing overload to RenderCopyExF

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -3089,34 +3089,6 @@ namespace SDL2
 			IntPtr dstrect
 		);
 
-		/* renderer refers to an SDL_Renderer*, texture to an SDL_Texture* */
-		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-		public static extern int SDL_RenderCopyEx(
-			IntPtr renderer,
-			IntPtr texture,
-			ref SDL_Rect srcrect,
-			ref SDL_FRect dstrect,
-			double angle,
-			ref SDL_FPoint center,
-			SDL_RendererFlip flip
-		);
-
-		/* renderer refers to an SDL_Renderer*, texture to an SDL_Texture*.
-		 * Internally, this function contains logic to use default values when
-		 * source, destination, and/or center are passed as NULL.
-		 * This overload allows for IntPtr.Zero (null) to be passed for srcrect.
-		 */
-		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-		public static extern int SDL_RenderCopyEx(
-			IntPtr renderer,
-			IntPtr texture,
-			IntPtr srcrect,
-			ref SDL_FRect dstrect,
-			double angle,
-			ref SDL_FPoint center,
-			SDL_RendererFlip flip
-		);
-
         /* renderer refers to an SDL_Renderer*, texture to an SDL_Texture*.
 		 * Internally, this function contains logic to use default values when
 		 * source, destination, and/or center are passed as NULL.
@@ -3126,6 +3098,21 @@ namespace SDL2
             IntPtr renderer,
             IntPtr texture,
             ref SDL_Rect srcrect,
+            ref SDL_FRect dstrect,
+            double angle,
+            ref SDL_FPoint center,
+            SDL_RendererFlip flip
+        );
+
+        /* renderer refers to an SDL_Renderer*, texture to an SDL_Texture*.
+		 * Internally, this function contains logic to use default values when
+		 * source, destination, and/or center are passed as NULL.
+		 */
+        [DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SDL_RenderCopyExF(
+            IntPtr renderer,
+            IntPtr texture,
+			IntPtr srcrect,
             ref SDL_FRect dstrect,
             double angle,
             ref SDL_FPoint center,
